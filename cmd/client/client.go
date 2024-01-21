c94ced8c76597f7e983e3a59ce77603724ee6f01
Mikhail Ulianov <zMikhailU@pttep.com>
2024-01-21 21:03:20 +0100

    Add logging of client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -9,6 +9,8 @@ import (
 	"github.com/btbph/word_of_wisdom/internal/dto/request"
 	"github.com/btbph/word_of_wisdom/internal/dto/response"
 	"github.com/btbph/word_of_wisdom/internal/hashcash"
+	"github.com/btbph/word_of_wisdom/internal/logger"
+	"log/slog"
 	"net"
 	"strings"
 )
@@ -19,53 +21,63 @@ func main() {
 		panic(err)
 	}
 
+	l, err := logger.Init(cfg.Client.LogLevel)
+	if err != nil {
+		panic(err)
+	}
+
 	conn, err := net.Dial("tcp", cfg.Client.DestinationAddress)
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close()
+	defer func() {
+		if err = conn.Close(); err != nil {
+			l.Error("failed to close connection", "error", err)
+		}
+	}()
 
-	if err = requestChallenge(conn); err != nil {
-		// log error
+	if err = requestChallenge(conn, l); err != nil {
 		return
 	}
 
-	challenge, err := recieveChallenge(conn)
+	challenge, err := recieveChallenge(conn, l)
 	if err != nil {
 		return
 	}
 
-	if err = sendChallengeSolution(conn, challenge, resource(cfg.Client.DestinationAddress)); err != nil {
+	if err = sendChallengeSolution(conn, challenge, resource(cfg.Client.DestinationAddress), l); err != nil {
 		return
 	}
 
-	quote, err := recieveQuote(conn)
+	quote, err := recieveQuote(conn, l)
 	if err != nil {
 		return
 	}
-
-	fmt.Printf("Quote recieved: %s\n", quote)
+	l.Info(fmt.Sprintf("Quote recieved: %s", quote))
 }
 
-func requestChallenge(conn net.Conn) error {
+func requestChallenge(conn net.Conn, logger *slog.Logger) error {
 	req := request.NewRequestChallenge()
 	bytes, err := json.Marshal(req)
 	if err != nil {
-		return err
+		logger.Error("failed to marshall request challenge request", "error", err)
+		return fmt.Errorf("failed to marshall request challenge request: %w", err)
 	}
 
 	_, err = conn.Write(bytes)
 	if err != nil {
-		return err
+		logger.Error("failed to write connection", "error", err)
+		return fmt.Errorf("failed to write connection: %w", err)
 	}
 
 	return nil
 }
 
-func recieveChallenge(conn net.Conn) (response.RequestChallenge, error) {
+func recieveChallenge(conn net.Conn, logger *slog.Logger) (response.RequestChallenge, error) {
 	res := response.RequestChallenge{}
 	if err := json.NewDecoder(conn).Decode(&res); err != nil {
-		return response.RequestChallenge{}, err
+		logger.Error("failed to decode reqest challenge response", "error", err)
+		return response.RequestChallenge{}, fmt.Errorf("failed to decode reqest challenge response: %w", err)
 	}
 
 	return res, nil
@@ -75,23 +87,30 @@ func resource(destinationAddress string) string {
 	return strings.Split(destinationAddress, ":")[0]
 }
 
-func sendChallengeSolution(conn net.Conn, challenge response.RequestChallenge, resource string) error {
+func sendChallengeSolution(conn net.Conn, challenge response.RequestChallenge, resource string, logger *slog.Logger) error {
 	h := hashcash.NewHashcash(challenge.ZeroBits, challenge.SaltLength, clock.New(), sha256.New())
 	req := request.NewSolutionProvided(h.Generate(resource))
 
 	bytes, err := json.Marshal(req)
 	if err != nil {
+		logger.Error("failed to marshall solution provided response", "error", err)
 		return err
 	}
 
 	_, err = conn.Write(bytes)
-	return err
+	if err != nil {
+		logger.Error("failed to write solution provided response", "error", err)
+		return fmt.Errorf("failed to write solution provided response: %w", err)
+	}
+
+	return nil
 }
 
-func recieveQuote(conn net.Conn) (string, error) {
+func recieveQuote(conn net.Conn, logger *slog.Logger) (string, error) {
 	res := response.SolutionProvided{}
 	if err := json.NewDecoder(conn).Decode(&res); err != nil {
-		return "", err
+		logger.Error("failed to decode solution provided response", "error", err)
+		return "", fmt.Errorf("failed to decode solution provided response: %w", err)
 	}
 
 	return res.Quote, nil
