16f3ade95dc4dadb6e97fcd284f812f869946a37
Mikhail Ulianov <zMikhailU@pttep.com>
2024-01-21 21:14:10 +0100

    Introduce getting random quotes

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -6,6 +6,7 @@ import (
 	"github.com/btbph/word_of_wisdom/internal/data_structures"
 	"github.com/btbph/word_of_wisdom/internal/dto"
 	"github.com/google/uuid"
+	"math/rand"
 )
 
 var ChallengeInfoNotFound = errors.New("challenge info not found")
@@ -13,12 +14,21 @@ var ChallengeInfoNotFound = errors.New("challenge info not found")
 type Repo struct {
 	challengesInfo      *data_structures.ConcurrentMap[uuid.UUID, dto.ChallengeInfo]
 	usedHashcashStrings *data_structures.ConcurrentSet[string]
+	quotes              []string
 }
 
 func NewRepo() *Repo {
 	return &Repo{
 		challengesInfo:      data_structures.NewConcurrentMap[uuid.UUID, dto.ChallengeInfo](),
 		usedHashcashStrings: data_structures.NewConcurrentSet[string](),
+		quotes: []string{
+			"Guard well your thoughts when alone and your words when accompanied.",
+			"I like to listen. I have learned a great deal from listening carefully. Most people never listen.",
+			"I think, that if the world were a bit more like ComicCon, it would be a better place.",
+			"We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.",
+			"The older I get, the greater power I seem to have to help the world; I am like a snowball - the further I am rolled the more I gain.",
+			"Knowledge is love and light and vision",
+		},
 	}
 }
 
@@ -47,5 +57,6 @@ func (r *Repo) SaveSolution(_ context.Context, solution string) error {
 }
 
 func (r *Repo) RandomQuote(_ context.Context) (string, error) {
-	return "Random quote!", nil
+	index := rand.Intn(len(r.quotes))
+	return r.quotes[index], nil
 }
